f0e1f539cd0836eb20d39b3e71d9163e1de68a28
Viktor Kocur <kocurvik@gmail.com>
2025-02-24 23:41:44 +0100

    fix reproj calib

--- PoseLib/robust/estimators/relative_pose.cc
+++ PoseLib/robust/estimators/relative_pose.cc
@@ -569,6 +569,7 @@ void RelativePoseMonoDepthEstimator::refine_model(CameraPose *pose) const {
         bundle_opt.loss_scale = opt.max_reproj_error;
         if (opt.optimize_shift) {
             refine_calib_abspose_shift(x1, x2, sigmas, pose, bundle_opt);
+            return;
         }
         bundle_adjust(x2, X1, pose, bundle_opt);
         return;
