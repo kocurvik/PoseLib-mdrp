4faa0498c75c78310f26375ca7444f93ffc0841f
yaqding <dingyaqing@163.com>
2025-02-26 16:15:40 +0100

    refined s00f solver

--- PoseLib/solvers/shared_focal_reldepth_relpose.cc
+++ PoseLib/solvers/shared_focal_reldepth_relpose.cc
@@ -293,27 +293,100 @@ void shared_focal_s00f_relpose(const std::vector<Eigen::Vector2d> &x1, const std
     b[10] = a[12] - a[13];
     b[11] = a[15] - a[16];
 
+    double c[18];
+    c[0] = -std::pow(b[11], 2);
+    c[1] = std::pow(b[2], 2);
+    c[2] = -std::pow(b[9], 2) - std::pow(b[10], 2);
+    c[3] = std::pow(b[0], 2) + std::pow(b[1], 2);
+    
+    c[4] = -1.0;
+    c[5] = 2 * a[15];
+    c[6] = -std::pow(a[15], 2);
+    c[7] = std::pow(b[5], 2);
+    c[8] = -std::pow(a[11], 2) - std::pow(a[14], 2);
+    c[9] = 2 * a[9] * a[11] + 2 * a[12] * a[14];
+    c[10] = -std::pow(a[9], 2) - std::pow(a[12], 2);
+    c[11] = std::pow(b[3], 2) + std::pow(b[4], 2);
+    
+    c[12] = 2 * a[16] - 2 * a[15];
+    c[13] = std::pow(a[15], 2) - std::pow(a[16], 2);
+    c[14] = std::pow(b[8], 2) - std::pow(b[5], 2);
+    c[15] = 2 * a[10] * a[11] - 2 * a[9] * a[11] - 2 * a[12] * a[14] + 2 * a[13] * a[14];
+    c[16] = std::pow(a[9], 2) - std::pow(a[10], 2) + std::pow(a[12], 2) - std::pow(a[13], 2);
+    c[17] = -std::pow(b[3], 2) - std::pow(b[4], 2) + std::pow(b[6], 2) + std::pow(b[7], 2);
 
+    double d[21];
 
-    Eigen::VectorXd datain(18);
-    datain << -b[11] * b[11], b[2] * b[2], -b[9] * b[9] - b[10] * b[10], b[0] * b[0] + b[1] * b[1],
-        -1.0, 2 * a[15], -a[15] * a[15], b[5] * b[5], -a[11] * a[11] - a[14] * a[14],
-        2.0 * a[9] * a[11] + 2.0 * a[12] * a[14], -a[9] * a[9] - a[12] * a[12], b[3] * b[3] + b[4] * b[4],
-        2.0 * a[16] - 2.0 * a[15], a[15] * a[15] - a[16] * a[16], b[8] * b[8] - b[5] * b[5],
-        2.0 * a[10] * a[11] - 2.0 * a[9] * a[11] - 2.0 * a[12] * a[14] + 2.0 * a[13] * a[14],
-        a[9] * a[9] - a[10] * a[10] + a[12] * a[12] - a[13] * a[13],
-        -b[3] * b[3] - b[4] * b[4] + b[6] * b[6] + b[7] * b[7];
+    d[6] = 1 / (a[6] - a[7]);
+    d[0] = (-c[3] * c[8]) * d[6];
+    d[1] = (-c[3] * c[9]) * d[6];
+    d[2] = (c[2] * c[11] - c[3] * c[10]) * d[6];
+    d[3] = (-c[3] * c[4] - c[1] * c[8]) * d[6];
+    d[4] = (-c[3] * c[5] - c[1] * c[9]) * d[6];
+    d[5] = (c[2] * c[7] - c[3] * c[6] + c[0] * c[11] - c[1] * c[10]) * d[6];
+    d[7] = (a[6] * a[16] - 2 * a[6] * a[15] + a[7] * a[15] + a[8] * a[15] - a[8] * a[16]) * d[6];
+    
+    d[8] = 1 / (2 * (a[6] - a[7]) * (a[15] - a[16]));
+    d[9] = (-c[3] * c[15]) * d[8];
+    d[10] = (c[2] * c[17] - c[3] * c[16]) * d[8];
+    d[11] = (-c[3] * c[12] - c[1] * c[15]) * d[8];
+    d[12] = (c[2] * c[14] - c[3] * c[13] + c[0] * c[17] - c[1] * c[16]) * d[8];
+    
+    d[13] = 1 / (a[6] + a[7] - 2 * a[8]);
+    d[14] = (a[8] * a[15] - a[7] * a[15] - a[6] * a[16] + a[8] * a[16]) * d[13];
+    d[15] = (c[8] * c[17]) * d[13];
+    d[16] = (c[9] * c[17] - c[11] * c[15]) * d[13];
+    d[17] = (c[10] * c[17] - c[11] * c[16]) * d[13];
+    d[18] = (c[4] * c[17] + c[8] * c[14]) * d[13];
+    d[19] = (c[5] * c[17] - c[7] * c[15] + c[9] * c[14] - c[11] * c[12]) * d[13];
+    d[20] = (c[6] * c[17] - c[7] * c[16] + c[10] * c[14] - c[11] * c[13]) * d[13];
 
+    Eigen::MatrixXd C0(3, 3);
+    C0 << d[2], d[5], d[7],
+          d[10], d[12], 1.0,
+          d[17], d[20], d[14];
 
-    Eigen::MatrixXd sols;
-    sols = solver_p3p_s00f(datain);
+    Eigen::MatrixXd C1(3, 2);
+    C1 << d[0]-d[9],  d[3]-d[11], d[1]-d[10],  d[4]-d[12];
+            0,     0, d[9],     d[11];
+            d[15]-d[9], d[18]-d[11], d[16]-d[10], d[19]-d[12];
 
+    Eigen::MatrixXd C2 = -C0.partialPivLu().solve(C1);
 
-    for (int k = 0; k < sols.cols(); ++k) {
-        double s = sols(0, k);
-        double f = sols(1, k);
-        double d3 = sols(2, k);
+    Eigen::MatrixXd AM(4, 4);
+    AM << 0, 0, 1.0, 0,
+          0, 0, 0, 1.0,
+          C2(0,0), C2(0,1), C2(0,2), C2(0,3),
+          C2(1,0), C2(1,1), C2(1,2), C2(1,3);
 
+    Eigen::EigenSolver<Eigen::Matrix<double, 2, 2>> es(AM, false);
+    Eigen::ArrayXcd D = es.eigenvalues();
+
+    for (int k = 0; k < 4; ++k) {
+        
+        if (abs(D(k).imag()) > 0.001 || D(k).real() < 0.0)
+            continue;
+
+        double d3 = 1.0 / D(k).real();
+
+        Eigen::MatrixXd A0(2, 2);
+        A0 << (d[3]-d[11])*d3*d3 + (d[4]-d[12])*d3 + d[5], d[7],
+                d[12] + d[11]*d3, 1.0;
+
+        Eigen::Vector2d A1;
+        A1 << (d[0]-d[9])*d3*d3 + (d[1]-d[10])*d3 + d[2], d[10] + d[9]*d3;
+        Eigen::Vector2d A2 = -A0.partialPivLu().solve(A1);
+
+        if (A2(0) < 0.0)
+            continue;
+
+        double s2 = -(c[1]*A2(0) + c[3])/(c[0]*A2(0) + c[2]);
+        if (s2 < 0.001)
+            continue;
+
+        double s = std::sqrt(s2);
+        double f = std::sqrt(A2(0));
+        
         Eigen::Matrix3d Kinv;
         Kinv << 1.0 / f, 0, 0, 0, 1.0 / f, 0, 0, 0, 1;
 
